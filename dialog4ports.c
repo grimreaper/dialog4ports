b018e99bb5d469793d804cc9166e591e11394633
eitan <eitan@localhost>
2010-11-03 10:53:54 -0400

    get the basic parsing down - get the linked list down

--- dialog4ports.c
+++ dialog4ports.c
@@ -1,4 +1,69 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct list_el {
+	char *name;
+	char *options;
+	struct list_el *next;
+};
+
+typedef struct list_el item;
+
 int main(int argc, char* argv[])
 {
+	item *curr;
+	item *head;
+	item *prev;
+
+//	head = (item *)malloc(sizeof(item));
+	prev = NULL;
+	head = NULL;
+	curr = NULL;
+
+	for (int arg=1; arg < argc; ++arg)
+	{
+		char *token;
+		bool gotName = false;
+		while ((token = strsep(&(argv[arg]), " \t")) != NULL)
+		{
+			curr = (item *)malloc(sizeof(item));
+			if (!head)
+			{
+				head = curr;
+			}
+			if (prev)
+			{
+				prev->next = curr;
+			}
+			char *internal_token;
+			while((internal_token = strsep(&token, "=")) != NULL)
+			{
+				if (!gotName)
+				{
+					printf("Setting name to %s\n", internal_token);
+					curr->name = internal_token;
+					gotName = true;
+
+				}
+				else
+				{
+					printf("Setting option to %s\n", internal_token);
+					curr->options = internal_token;
+				}
+			}
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+
+	curr = head;
+
+	while(curr) {
+		printf("%s support %s \n", curr->name, curr->options);
+		curr = curr->next;
+	}
+
 	return 0;
 }
