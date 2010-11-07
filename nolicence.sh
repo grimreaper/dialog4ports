4e89b914c58d11a734d47429ffb4a8abf40ddca4
eitan <eitan@localhost>
2010-11-07 00:44:06 -0400

    add new testing files

---
+++ nolicence.sh
@@ -0,0 +1,9 @@
+testingPort="/usr/ports/x11-wm/icewm"
+
+WITH_XORG=WITH_XORG	./dialog4ports	$(make -C $testingPort -V PORTNAME)="$(make -C $testingPort -V COMMENT)"  \
+				WITH_XORG='use xorg'=% \
+				WITHOUT_FOO='do not use foo'=%=info.txt \
+				WWW_TYPE='use this for a web server!'=apache#lighttpd#god#heaven \
+				LANGS='use a language '=-=n.txt \
+				XYZ='argi'=-		\
+				AAAAZ='hello my dear'=%
