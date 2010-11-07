dddb95c930b911d4aeeac6d85055e457be8047d7
eitan <eitan@localhost>
2010-11-06 22:47:22 -0400

    add port comment testing stuff

--- foo.sh
+++ foo.sh
@@ -1,4 +1,6 @@
-WITH_XORG=WITH_XORG	./dialog4ports	portname \
+testingPort="/usr/ports/x11-wm/icewm"
+
+WITH_XORG=WITH_XORG	./dialog4ports	$(make -C $testingPort -V PORTNAME)="$(make -C $testingPort -V COMMENT)"  \
 				WITH_XORG='use xorg'=% \
 				WITHOUT_FOO='do not use foo'=% \
 				WWW_TYPE='use this for a web server!'=apache#lighttpd#god#heaven \
